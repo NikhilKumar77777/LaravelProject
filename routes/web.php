a6c8cb4a5f33f759f94aeb4ccb6e29825143e624
NikhilKumar77777 <nkup243724@gmail.com>
2023-03-05 02:56:45 +0530

    In this project I work php laravel laravel-framework crud-application add-product custom-authentication(MVC Model)

---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/home/add', [ProductController::class, 'index'])->name('addproduct');
+// Route::get('/', function () {
+//     return view('reload');
+// });
+// Route::view('/','insert');
+
+Route::post('/home/add',[ProductController::class ,'insert'])->name('home.add');
+
+Route::view('update','updateview');
+Route::get('updatedelete',[ProductController::class ,'updateordelete']);
+Route::get('updateData',[ProductController::class ,'update']);
+Route::get('/add', function () {
+    
+    return view('add');
+});
+
+
